6299e4b67bd2be02936ea06d5b00bf7d55e712b6
SteffRhes <steffrhes@pm.me>
2025-04-16 10:27:15 +0200

    modify docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -38,26 +38,26 @@ docker_create_db_directories() {
 
 	mkdir -p "$PGDATA"
 	# ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
-	chmod 00700 "$PGDATA" || :
+  #chmod 00700 "$PGDATA" || :
 
 	# ignore failure since it will be fine when using the image provided directory; see also https://github.com/docker-library/postgres/pull/289
 	mkdir -p /var/run/postgresql || :
-	chmod 03775 /var/run/postgresql || :
+	#chmod 03775 /var/run/postgresql || :
 
 	# Create the transaction log directory before initdb is run so the directory is owned by the correct user
 	if [ -n "${POSTGRES_INITDB_WALDIR:-}" ]; then
 		mkdir -p "$POSTGRES_INITDB_WALDIR"
-		if [ "$user" = '0' ]; then
-			find "$POSTGRES_INITDB_WALDIR" \! -user postgres -exec chown postgres '{}' +
-		fi
-		chmod 700 "$POSTGRES_INITDB_WALDIR"
+		#if [ "$user" = '0' ]; then
+		#  find "$POSTGRES_INITDB_WALDIR" \! -user postgres -exec chown postgres '{}' +
+		#fi
+		#chmod 700 "$POSTGRES_INITDB_WALDIR"
 	fi
 
 	# allow the container to be started with `--user`
-	if [ "$user" = '0' ]; then
-		find "$PGDATA" \! -user postgres -exec chown postgres '{}' +
-		find /var/run/postgresql \! -user postgres -exec chown postgres '{}' +
-	fi
+	#if [ "$user" = '0' ]; then
+	#  find "$PGDATA" \! -user postgres -exec chown postgres '{}' +
+	#  find /var/run/postgresql \! -user postgres -exec chown postgres '{}' +
+	#fi
 }
 
 # initialize empty PGDATA directory with new database via 'initdb'
@@ -311,10 +311,10 @@ _main() {
 		docker_setup_env
 		# setup data directories and permissions (when run as root)
 		docker_create_db_directories
-		if [ "$(id -u)" = '0' ]; then
-			# then restart script as postgres user
-			exec gosu postgres "$BASH_SOURCE" "$@"
-		fi
+		#if [ "$(id -u)" = '0' ]; then
+		#  # then restart script as postgres user
+		#  exec gosu postgres "$BASH_SOURCE" "$@"
+		#fi
 
 		# only run initialization on an empty data directory
 		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
